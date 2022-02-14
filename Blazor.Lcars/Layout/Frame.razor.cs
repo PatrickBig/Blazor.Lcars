6d5e66d7bc5abd9d7845a50b915e545fa6ffc5df
Patrick Bigler <patrick.bigler@ideaintegrations.com>
2022-02-14 17:21:56 -0600

    Starting some components

---
+++ Blazor.Lcars/Layout/Frame.razor.cs
@@ -0,0 +1,19 @@
+﻿using Blazor.Lcars.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Lcars
+{
+    public partial class Frame : LcarsBase
+    {
+        [Parameter]
+        public FrameHorizontal Horizontal { get; set; }
+
+        [Parameter]
+        public FrameSide Side { get; set; }
+    }
+}
